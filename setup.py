2b259537b8a98f26138fadab510a53f99b6dfce8
Jarrod J Manzer <jjmanzer@godaddy.com>
2020-03-30 14:21:42 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='netbox-loadtest',
+    version='1.0.0',
+    url='https://github.com/jjmanzer/netbox-loadtest/',
+    author='Jarrod Manzer',
+    author_email='jarrod.manzer@gmail.com',
+    license='Apache',
+    description='a load test script for the netbox IPAM solution',
+    include_package_data=True,
+    scripts=['bin/netbox-loadtest.py'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    zip_safe=False,
+    setup_requires=[],
+    tests_require=[],
+)
