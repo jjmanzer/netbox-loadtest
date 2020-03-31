0ecdea000f8e4e25cef08683321e4012b8cbb6ef
Jarrod J Manzer <jjmanzer@godaddy.com>
2020-03-30 18:32:04 -0700

    add some tests

--- tests/test_netbox_loadtest.py
+++ tests/test_netbox_loadtest.py
@@ -3,7 +3,13 @@ import unittest
 from bin.netbox_loadtest import colnum_string
 
 
-class TestColNumString(unittest.TestCase):
+class TestNetBoxKLoadTest(unittest.TestCase):
 
-    def test_colnum_string_valid(self):
+    def test_colnum_string_valid_singleletter(self):
+        self.assertEqual('A', colnum_string(1))
+
+    def test_colnum_string_valid_two_letter(self):
         self.assertEqual('AA', colnum_string(27))
+
+    def test_colnum_string_valid_three_letter(self):
+        self.assertEqual('AAA', colnum_string(703))
